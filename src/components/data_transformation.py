542e6e9475d1ea129adc7944c59cc066fc27524d
Samarthaga-Git <samarthagarwal12345@gmail.com>
2023-05-24 17:48:41 +0530

    Final Changes Before pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -83,11 +83,14 @@ class DataTransformation:
             preprocessing_obj = self.get_data_transformation_object()
 
             target_column_name = 'price'
-            drop_columns = [target_column_name,'id']
-
+            drop_columns = [target_column,'id']
+            
+            # Dividing the dataset into independent and dependent features 
+            ## Training Data
             input_feature_train_df = train_df.drop(columns=drop_columns,axis=1)
             target_feature_train_df=train_df[target_column_name]
 
+            #test Data 
             input_feature_test_df=test_df.drop(columns=drop_columns,axis=1)
             target_feature_test_df=test_df[target_column_name]
             
--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -13,6 +13,6 @@ if __name__=='__main__':
     train_data_path,test_data_path=obj.initiate_data_ingestion()
     print(train_data_path,test_data_path)
     data_transformation =  DataTransformation()
-    train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
+    train_arr,test_arr,obj_path=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
     model_trainer=ModelTrainer()
     model_trainer.initate_model_training(train_arr,test_arr)
